021585d1a22d1d409052877aa3c93485cbb3b642
aylenindaws <aylen@indaws.es>
2024-08-06 04:01:51 +0200

    [ADD] meraik expense conection

---
+++ indaws_meraik_conection_expense/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "MerAik Indaws Purchase Order",
+    "summary": "MerAik Indaws Purchase Order",
+    "description": "This module allows you to connect Purchase Orders with MerAik",
+    "version": "17.0.1.1.0",
+    "category": "HR",
+    "license": "AGPL-3",
+    "website": "https://www.indaws.es/",
+    "author": "MerAik, Aylen Garces, inDAWS",
+    "depends": ["hr_expense","indaws_meraik_conection_base"],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
