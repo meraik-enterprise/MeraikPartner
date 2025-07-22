3d540b3660ea995e80b15e87623de03880adab2e
aylenindaws <aylen@indaws.es>
2025-07-22 08:33:10 +0200

    [UPD] clean main

--- meraik_livechat/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo.
-# Copyright 2024 Bright3C - Aylen Garcés Fernandez
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import models
--- meraik_livechat/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo.
-# Copyright 2024 Bright3C - Aylen Garcés Fernandez
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import chatbot_script_step
-from . import discuss_channel
-from . import livechat_channel
